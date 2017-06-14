8325bd3bd51081f5faa1837fee532ffa2fec84f7
Bart Wijnants <bartwijnants@outlook.com>
2017-06-14 21:45:16 +0200

    add lint-staged

--- src/foo.test.js
+++ src/foo.test.js
@@ -4,4 +4,4 @@ test("remove a semicolon and commit, you will see that prettier automatically in
 
 test("change one 4 to 5 and try to commit, this will not work.", () => {
     expect(4).toBe(4);
-})
+});
