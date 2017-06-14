5ff95251b0deaa1f4e20e4f7dfed613c8e436bb3
Bart Wijnants <bartwijnants@outlook.com>
2017-06-14 20:13:10 +0200

    initial commit

---
+++ src/foo.test.js
@@ -0,0 +1,7 @@
+test('remove a semicolon and commit, you will see that prettier automatically inserts the semicolon.', () => {
+    expect(4).toBe(4);
+});
+
+test('change one 4 to 5 and try to commit, this will not work.', () => {
+    expect(4).toBe(4);
+});
