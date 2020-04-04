29d75dbe20704998d711bd00c273aabc275fb376
Bart Wijnants <bart.w@sparkcentral.com>
2020-04-04 08:32:49 +0200

    retest precommit hook

--- src/foo.test.js
+++ src/foo.test.js
@@ -3,5 +3,5 @@ test("remove a semicolon and commit, you will see that prettier automatically in
 });
 
 test("change one 4 to 5 and try to commit, this will not work.", () => {
-    expect(4).toBe(4);
+    expect(4).toBe(5);
 });
