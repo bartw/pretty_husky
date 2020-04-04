61a6475d3bc1b48b27e772dd1284b153e8441ba4
Bart Wijnants <bart.w@sparkcentral.com>
2020-04-04 08:31:21 +0200

    test pretty hook

--- src/foo.test.js
+++ src/foo.test.js
@@ -1,5 +1,5 @@
 test("remove a semicolon and commit, you will see that prettier automatically inserts the semicolon.", () => {
-    expect(4).toBe(4);
+    expect(4).toBe(4)
 });
 
 test("change one 4 to 5 and try to commit, this will not work.", () => {
